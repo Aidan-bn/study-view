0b25d62beb67f1548dae6f794ab811527e25b2ed
Aidan-bn <bantezea@gmail.com>
2024-04-05 06:26:03 +0300

    Install moongose, dotenv and express packages

--- class-db/index.js
+++ class-db/index.js
@@ -1,1 +1,8 @@
-const app = require('express');
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose')
+const classRoutes = require('./routes/class')
+
+const app = express();
+
+
