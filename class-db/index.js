596722864e303f736e51c5ec57799d43246e3b07
Aidan-bn <bantezea@gmail.com>
2024-04-09 10:12:41 +0300

    Establish a database connection

--- class-db/index.js
+++ class-db/index.js
@@ -1,23 +1,23 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose')
-const userRoutes = require('./routes/user')
+// const userRoutes = require('./routes/user')
 
 const app = express();
 
 //middleware setup
 app.use(express.json())
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+// app.use((req, res, next) => {
+//     console.log(req.path, req.method)
+//     next()
+// })
 
 //routes
-app.use('/api/user', userRoutes)
+// app.use('/api/user', userRoutes)
 
 //database connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log('connected to database and listening on port', process.env.PORT)
